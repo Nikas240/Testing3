6dc17b86bf0b3ce675e31b8e9e86f1f588160375
Nikas240 <61223884+Nikas240@users.noreply.github.com>
2021-04-28 16:05:13 +0300

    Update and rename functions.h to tetrismodule.h

--- app/tetrismodule.h
+++ app/tetrismodule.h
@@ -1,8 +1,8 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
+#ifndef TETRISMODULE_H
+#define TETRISMODULE_H
 struct Point
 {int x,y;} a[4], b[4];
 void cleararray(int field[20][10]);
 int calculate(int sum, int k);
 int check(int field[20][10]);
-#endif // MYFUNC_H
+#endif 
